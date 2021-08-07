d3e2e8c1c95af79ab409b7c386fb290ea96a1f41
mkrkru <mkrkru@mail.ru>
2021-08-07 16:02:41 +0300

    update css

--- extension/scripts/popup.js
+++ extension/scripts/popup.js
@@ -1,9 +1,26 @@
 window.onload = () => {
-    document.getElementById("popupModalButton").addEventListener("click", testbutton);
+    document.getElementById("popupModalButtonSend").addEventListener("click", sendData);
+    document.getElementById("popupModalButtonGet").addEventListener("click", getData);
 }
 
-function testbutton() {
-    fetch("http://192.168.239.83:8080/", {
+function sendData() {
+    fetch("http://192.168.239.83:8080/add", {
+        method: 'POST',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "image": "https://steamuserimages-a.akamaihd.net/ugc/1752432259979938956/1CABED7C89C38A6E2B9F7F798F1FC7A2380842A3/?imw=512&amp;imh=341&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=true", // url-адрес картинки баннера
+            "url": "yandex.ru", // url-адрес, ведущий на их сайт
+            "domens": ["stackoverflow.com"] // домены, на которых показывать рекламу
+        })
+    })
+}
+
+function getData() {
+    fetch("http://192.168.239.83:8080", {
         method: 'GET',
         headers: {
             'Access-Control-Allow-Origin': '*',
@@ -12,5 +29,5 @@ function testbutton() {
         }
     })
         .then(res => res.json())
-        .then(res => console.log(res))
+        .then(res => { return res })
 }
